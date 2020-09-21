1c422e25e9593773057bc3e695223c0d5d1a6354
vuonglv <it.vuonglv@gmail.com>
2020-09-21 22:36:39 +0700

    add session context

--- src/app/db/session.py
+++ src/app/db/session.py
@@ -1,8 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 from src.app.config import settings
 
 
 engine = create_engine(settings.POSTGRES_URL)
 Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
