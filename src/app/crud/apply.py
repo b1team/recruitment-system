384d9a0d2d40f42f9eef41634f19e446cd6c4137
tungnt <tungnt@pop-os.localdomain>
2020-09-25 21:36:06 +0700

    feature apply

--- src/app/crud/apply.py
+++ src/app/crud/apply.py
@@ -1,9 +1,13 @@
 from src.app.models.apply import Apply
-from src.app.schemas.apply import ApplyBase
+from src.app.schemas.apply import ApplyInDB
 
 from sqlalchemy.orm.session import Session
 
 
-class CRUDemployee:
+class CRUDapply:
     def __init__(self, session):
         self.db = session
+
+    def create(self, apply: ApplyInDB):
+        new_apply = Apply(**apply.dict())
+        self.db.add(new_apply)
--- src/app/schemas/apply.py
+++ src/app/schemas/apply.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
 
-from src.app.constants import UserType
-
 
 class ApplyBase(BaseModel):
     job_id: int
     description: Optional[str] = None
     cv: str
-    status: UserType
+    status: Optional[str] = 'pending'
 
 
 class ApplyInDB(ApplyBase):
