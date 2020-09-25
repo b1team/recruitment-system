f57ebd5f60075c47d438b606c559097188f5edb2
tungnt <tungnt@pop-os.localdomain>
2020-09-25 13:05:59 +0700

    handle not found exception

--- src/app/api/v1/endpoints/tags.py
+++ src/app/api/v1/endpoints/tags.py
@@ -7,22 +7,23 @@ from src.app.db.session import session_scope
 from src.app.schemas.tag import TagBase, PayloadTag
 
 from src.app.crud.tag import CRUDTag
+from src.app.exceptions import JobNotFoundError
 
 router = APIRouter()
 
 
-def add_job_tag(db, job_id, tags):
-    db_job = db.query(Job).filter(Job.id == job_id).first()
-    db_job.tags.extend(tags)
-
-
 @router.post('/tags/{job_id}')
-async def tags(job_id: int, payload: PayloadTag):
+async def create_tags(job_id: int, payload: PayloadTag):
     with session_scope() as db:
         crud = CRUDTag(db)
-        usable_tag = crud.filter(PayloadTag(**payload.dict()))
-        crud.create(usable_tag)
-        tag = crud.get(PayloadTag(**payload.dict()))
-        if tag:
-            add_job_tag(db, job_id, tag)
+        not_exist_tags = crud.get_not_exist_tags(payload.tags)
+        if not_exist_tags:
+            crud.create_many(not_exist_tags)
+        tags = crud.get_many(payload.tags)
+        if tags:
+            job = db.query(Job).get(job_id)
+            if not job:
+                raise JobNotFoundError(info={'job_id': job_id})
+            job.tags.extend(tags)
+
     return payload
--- src/app/crud/tag.py
+++ src/app/crud/tag.py
@@ -1,6 +1,5 @@
 from src.app.models.tag import Tag
 from sqlalchemy.orm.session import Session
-from src.app.schemas.tag import TagBase, PayloadTag
 
 from typing import List
 
@@ -8,17 +7,16 @@ class CRUDTag:
     def __init__(self, session):
         self.db = session
     
-    def get(self, payload: PayloadTag):
-        tag = self.db.query(Tag).filter(Tag.name.in_(payload.tags))
-        return tag
+    def get_many(self, tags: List[str]):
+        return self.db.query(Tag).filter(Tag.name.in_(tags))
 
-    def create(self, tags: List[str]):
+    def create_many(self, tags: List[str]):
         for tag in tags:
             new_tag = Tag(name=tag)
             self.db.add(new_tag)
     
-    def filter(self, payload: PayloadTag):
+    def get_not_exist_tags(self, tags: List[str]):
         existing_tags = [result.name for result in self.db.query(Tag.name)
-                         .filter(Tag.name.in_(payload.tags))]
-        usable_tag = list(set(payload.tags) - set(existing_tags))
-        return usable_tag
+                         .filter(Tag.name.in_(tags))]
+        return list(set(tags) - set(existing_tags))
+        
