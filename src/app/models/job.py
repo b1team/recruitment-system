c9b155b9e6ed92cc82a1266b137d1f97520e9019
vuonglv <it.vuonglv@gmail.com>
2020-09-25 13:27:18 +0700

    add job status to model

--- src/app/models/job.py
+++ src/app/models/job.py
@@ -11,6 +11,7 @@ class Job(Base):
     salary = Column(types.Float, nullable=False)
     address = Column(types.Text, nullable=False)
     description = Column(types.Text, nullable=False)
+    is_open = Column(types.Boolean, nullable=False, default=True)
     tags = relationship("Tag", secondary=JobTag)
     applies = relationship("Apply", backref="job")
     employer_id = Column(types.Integer, ForeignKey("employers.id"), nullable=False)
